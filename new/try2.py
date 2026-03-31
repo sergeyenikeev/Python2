7c8b1e3ca3ebe5e642998e585bba1181318dfd98
sergeyenikeev <s.enikeev@gmail.com>
2026-03-31 12:10:46 +0300

    test

--- new/try2.py
+++ new/try2.py
@@ -2,9 +2,14 @@ try:
     # a = 10 / 0
     # a = int("abc")
     print("Hello")
+    # raise Exception("Моя ошибка")
 except ZeroDivisionError as e:
     print(f"Error {e}")
 except ValueError as e:
     print(f"Error {e}")
+except BaseException as e:
+    print(f"Ошибка: {e}")
+else:
+    print("else")
 finally:
     print("The end.")
